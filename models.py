09fc46cbed2258c81e2d2d3314a90af31d81c2bd
ali13 <ali13@student.gsu.edu>
2022-09-27 22:40:08 -0400

    testing HTTPS

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app_config import application
+from sqlalchemy import ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -12,6 +13,12 @@ class User(db.Model, UserMixin):
     picture = db.Column(db.Text, unique=False, nullable=True)
     account_id = db.Column(db.String(100), unique=True, nullable=False)
     google_id = db.Column(db.String(100), unique=True, nullable=True)
+
+
+# class Favorite(db.Model, UserMixin):
+#     __tablename__ = "Favorites"
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.String(100), ForeignKey("Users.account_id"))
 
 
 db.create_all()
